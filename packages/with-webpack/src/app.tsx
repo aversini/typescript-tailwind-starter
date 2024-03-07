03cd5e9ef78520f9777a846f4fb0ddc5a7dc1e54
Arno V <versini@gmail.com>
2024-03-07 17:54:51 -0400

    adding webpack

--- packages/with-webpack/src/app.tsx
+++ packages/with-webpack/src/app.tsx
@@ -2,6 +2,6 @@ import * as React from "react";
 
 export const App: React.FC = () => (
 	<div className="prose prose-dark">
-		<h1>Vite + TS + TailwindCSS</h1>
+		<h1>Webpack + TS + TailwindCSS</h1>
 	</div>
 );
---
+++ packages/with-webpack/tailwind.config.js
@@ -0,0 +1,7 @@
+/** @type {import('tailwindcss').Config} */
+
+import { twPlugin } from "@versini/ui-plugins";
+
+module.exports = twPlugin.merge({
+	content: ["./src/**/*.{js,ts,jsx,tsx}"],
+});
